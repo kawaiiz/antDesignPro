049757e12c74b515318c079aa7b6b01ed8f1b78a
kawaiiz <690902636@qq.com>
2019-09-10 20:40:28 +0800

    测试提交到公司码云

--- src/layouts/SecurityLayout.tsx
+++ src/layouts/SecurityLayout.tsx
@@ -88,7 +88,7 @@ const SecurityLayoutFunc: React.FC<SecurityLayoutProps> = props => {
   }
   // 构建 更新路由所需的 components 
   useState(() => {
-
+    console.log(1)
     if (route && route.routes) {
       const map = new Map<string, any>();
       mapRoute(route.routes[0].routes!, map)
